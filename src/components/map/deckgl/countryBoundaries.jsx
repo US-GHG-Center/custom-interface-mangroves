0e9cfd6cc0dbaa30a015d1eb19b2dc2cf45b9f5d
Ram Sharan Rimal <rsr0012@uah.edu>
2025-09-11 15:36:49 -0500

    tryout mapbox vector tiles

--- src/components/map/deckgl/countryBoundaries.jsx
+++ src/components/map/deckgl/countryBoundaries.jsx
@@ -1,55 +1,60 @@
 
 import { useMemo } from 'react';
-import { GeoJsonLayer } from '@deck.gl/layers';
+import { MVTLayer } from '@deck.gl/geo-layers';
+import { useConfig } from '../../../context/configContext';
 
 const DEFAULT_COLOR = [0, 0, 0, 0];
 const DEFAULT_LINE_COLOR = [40, 150, 225, 80];
 const HIGHLIGHT_LINE_COLOR = [30, 144, 255, 255];
 
 export function useCountryBoundaries({
-  countryWiseBoundaries,
+  countries = ['yemen'], // array of country names
   hoveredCountry,
   showBoundries
 }) {
+  const { config } = useConfig();
+  const mapboxToken = config?.mapboxToken
   const boundariesLayer = useMemo(() => {
-    if (!countryWiseBoundaries) {
+    if (!mapboxToken || !Array.isArray(countries) || countries.length === 0) {
       return null;
     }
-    return new GeoJsonLayer({
-      id: 'country-boundaries-layer',
-      data: countryWiseBoundaries,
-      // Style Properties
+    return new MVTLayer({
+      id: 'country-boundaries-mvt-layer',
+      data: `https://api.mapbox.com/v4/mapbox.country-boundaries-v1/{z}/{x}/{y}.vector.pbf?access_token=${mapboxToken}`,
+      visible: showBoundries,
+      pickable: true,
       stroked: true,
       filled: true,
-      visible: showBoundries,
       getLineColor: (feature) => {
         if (
           hoveredCountry &&
-          hoveredCountry.properties.NAME === feature.properties.NAME
+          hoveredCountry.properties?.name === feature.properties?.name
         ) {
           return HIGHLIGHT_LINE_COLOR;
         }
         return DEFAULT_LINE_COLOR;
       },
-
       getFillColor: DEFAULT_COLOR,
       getLineWidth: (feature) => {
         if (
           hoveredCountry &&
-          hoveredCountry.properties.NAME === feature.properties.NAME
+          hoveredCountry.properties?.name === feature.properties?.name
         ) {
-          return 4; // Highlighted width
+          return 4;
         }
-        return 2; // Default width
+        return 2;
       },
       lineWidthUnits: 'pixels',
-      pickable: true,
       updateTriggers: {
         getLineWidth: [hoveredCountry],
         getLineColor: [hoveredCountry],
       },
+      filterFeature: (feature) => {
+        // Only show boundaries for selected countries
+        return countries.includes(feature.properties?.name);
+      },
     });
-  }, [countryWiseBoundaries, showBoundries, hoveredCountry]);
+  }, [mapboxToken, countries, showBoundries, hoveredCountry]);
 
   return { boundariesLayer };
 }
